94b13ae943eb3577b0d07a73db85bee6aceed32f
Karim Vaes <karim@kvaes.be>
2017-05-07 19:39:26 +0200

    Updating the flow a bit to get the config of the 2nd dc working (intermediate - non working - commit)

--- Configurations/DscConfDomainController.ps1
+++ Configurations/DscConfDomainController.ps1
@@ -1,10 +1,11 @@
 Configuration DscConfDomainController
 {
-    Import-DscResource -ModuleName xActiveDirectory, xStorage, PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xActiveDirectory, xStorage, PSDesiredStateConfiguration, xDSCDomainjoin
     $dscDomainAdmin = Get-AutomationPSCredential -Name 'addcDomainAdmin'
 	$dscDomainName = Get-AutomationVariable -Name 'addcDomainName'
     $dscDomainNetbiosName = Get-AutomationVariable -Name 'addcDomainNetbiosName'
 	$dscSafeModePassword = $dscDomainAdmin
+	$DomainRoot = "DC=$($dscDomainAdmin -replace '\.',',DC=')"
 
     node FirstDC
     {
@@ -67,30 +68,98 @@ Configuration DscConfDomainController
             SysvolPath = "F:\SYSVOL"
 	        DependsOn = "[xDisk]ADDataDisk"
         } 
+		
+		xADUser FirstUser 
+        { 
+            DomainName = $dscDomainName 
+            DomainAdministratorCredential = $dscDomainAdmin
+            UserName = $dscDomainAdmin.Username 
+            Password = $dscDomainAdmin
+            Ensure = "Present" 
+            DependsOn = "[xADDomain]FirstDS" 
+        } 
+		
+		xADGroup FirstUserAddToAdmins
+		{
+			GroupName = "G_$RootOU"
+			GroupScope = 'Global'
+			Description = "Designated administrators of the domain"
+			Category = 'Security'
+			Members = $dscDomainAdmin.Username 
+			Path = "OU=Users,$DomainRoot"
+			Ensure = 'Present'
+			DependsOn = "[xADUser]FirstUser" 
+		}
     }      
 
     
     node AdditionalDC
     {
-		WindowsFeature ADDSInstall 
+
+	    WindowsFeature DNS 
         { 
             Ensure = "Present" 
-            Name = "AD-Domain-Services" 
-        } 
-        xWaitForADDomain DscForestWait 
+            Name = "DNS"		
+        }
+
+	    WindowsFeature DnsTools
+	    {
+	        Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+	    }
+
+        xWaitforDisk Disk2
+        {
+            DiskNumber = 2
+            RetryIntervalSec = 20
+            RetryCount = 30
+			DependsOn = "[WindowsFeature]ADAdminCenter" 
+        }
+
+        xDisk ADDataDisk {
+            DiskNumber = 2
+            DriveLetter = "F"
+            DependsOn = "[xWaitForDisk]Disk2"
+        }
+
+        WindowsFeature ADDSInstall 
         { 
-            DomainName = $dscDomainName
-            DomainUserCredential = $dscDomainAdmin 
-            RetryCount =  1440
-            RetryIntervalSec = 60
-            DependsOn = "[WindowsFeature]ADDSInstall" 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+	        DependsOn="[WindowsFeature]DNS" 
         } 
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+		
+        xDSCDomainjoin JoinDomain
+		{
+			Domain = $dscDomainName 
+			Credential = $dscDomainAdmin
+			DependsOn = "[xDisk]ADDataDisk"
+		}
+		
         xADDomainController SecondDC
         {
             DomainName = $dscDomainName
             DomainAdministratorCredential = $dscDomainAdmin
             SafemodeAdministratorPassword = $dscSafeModePassword
-			DependsOn = "[xWaitForADDomain]DscForestWait" 
+			DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+			DependsOn = "[xDSCDomainjoin]JoinDomain" 
         }
     }     
 
--- Modules/DscConfDomainController-Modules.ps1
+++ Modules/DscConfDomainController-Modules.ps1
@@ -1,4 +1,5 @@
 $modules = @{
     "xActiveDirectory" = "2.16.0.0"; 
-    "xstorage" = "2.9.0.0"
+    "xstorage" = "2.9.0.0";
+    "xDSCDomainjoin" = "1.1"
 }
